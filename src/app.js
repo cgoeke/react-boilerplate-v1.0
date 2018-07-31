531f2ee17cee33dd7575c7dad43778c3d72f56a4
Christian Goeke <christianpgoeke@gmail.com>
2018-07-31 11:05:40 -0700

    initial commit

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<App/>, document.getElementById('app'));
